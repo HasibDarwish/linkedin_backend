f26842acd1814576f6fb436129ac38dd078dff45
ALStoilkov <antonistoilkov@gmail.com>
2021-06-15 10:05:41 +0100

    first

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import cors from "cors";
+import profileRouter from "./service/profile.js";
+import {
+  badRequestHandler,
+  unAuthorizedHandler,
+  forBiddenHandler,
+  notFoundHandler,
+  catchAllHandler,
+} from "./errorHandler/index.js";
+
+const server = express();
+const { PORT, MONGO_CONNECTION_ATLAS } = process.env;
+
+server.use(express.json());
+server.use(cors());
+
+server.use("/api", profileRouter);
+
+server.use(badRequestHandler);
+server.use(unAuthorizedHandler);
+server.use(forBiddenHandler);
+server.use(notFoundHandler);
+server.use(catchAllHandler);
+
+mongoose
+  .connect(MONGO_CONNECTION_ATLAS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(
+    server.listen(PORT, () => {
+      console.table(listEndpoints(server));
+      console.table({ "Running At Port Number": PORT });
+    })
+  );
