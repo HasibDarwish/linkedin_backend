e5c94461d3b491f9ef483ce38949fb0680d75271
ALStoilkov <antonistoilkov@gmail.com>
2021-06-18 14:42:49 +0100

    profile put fixed

--- src/service/post.js
+++ src/service/post.js
@@ -1,177 +1,170 @@
 import { Router } from "express";
-import createError from "http-errors"
+import createError from "http-errors";
 import PostModel from "../schema/post.js";
 import ProfileModel from "../schema/profile.js";
-import multer from "multer"
-import { v2 as cloudinary } from "cloudinary"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const PostRouter = Router()
+const PostRouter = Router();
 
 //~~~~~~~~~~~~~~~~~~~~~~~ POSTS SECTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 PostRouter.post("/", async (req, res, next) => {
+  try {
+    const user = await ProfileModel.findById(req.body.user);
+    if (user) {
+      const newPost = new PostModel(req.body);
+      const savedPost = await newPost.save();
 
-    try {
-        const user = await ProfileModel.findById(req.body.user)
-        if (user) {
-            const newPost = new PostModel(req.body)
-            const savedPost = await newPost.save()
-
-            res.status(201).send(savedPost)
-        }
-        else {
-            next(createError(404, `User with id: ${req.body.user} not found!`))
-        }
-
-
-    } catch (error) {
-        console.log(error)
-        if (error.name === "ValdidationError") {
-            next(createError(400, error))
-        } else {
-            next(createError(500, "An error occurred while saving the post "))
-        }
-
+      res.status(201).send(savedPost);
+    } else {
+      next(createError(404, `User with id: ${req.body.user} not found!`));
     }
-})
+  } catch (error) {
+    console.log(error);
+    if (error.name === "ValdidationError") {
+      next(createError(400, error));
+    } else {
+      next(createError(500, "An error occurred while saving the post "));
+    }
+  }
+});
 
-// Posting images 
+// Posting images
 
-const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env
+const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
 cloudinary.config({
-    cloud_name: CLOUDINARY_NAME,
-    api_key: CLOUDINARY_KEY,
-    api_secret: CLOUDINARY_SECRET
-})
-
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
 
 // this comes from the library "multer-storage-cloudinary"
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: "Build_Week_3",
-        //     public_id: async (req, file) => {
-        // here i can choose a custom public_id 
-        //     }
-        use_filename: true
-    }
-
+  cloudinary: cloudinary,
+  params: {
+    folder: "Build_Week_3",
+    //     public_id: async (req, file) => {
+    // here i can choose a custom public_id
+    //     }
+    use_filename: true,
+  },
 });
-
 
 // https://dev.to/itsmefarhan/cloudinary-files-images-crud-operations-with-nodejs-express-multer-2147
 
-
-PostRouter.post("/withImage", multer({ storage }).single("cover"), async (req, res, next) => {
+PostRouter.post(
+  "/withImage",
+  multer({ storage }).single("cover"),
+  async (req, res, next) => {
     try {
+      const user = await ProfileModel.findById(req.body.user);
+      if (user) {
+        let fileNameWithSlash = req.file.filename;
+        let indexOfSlash = fileNameWithSlash.indexOf("/");
+        let fileNameWithoutSlash = fileNameWithSlash.slice(indexOfSlash + 1);
+        let body = {
+          text: req.body.text,
+          user: req.body.user,
+          image: req.file.path,
+          cloudinaryId: fileNameWithoutSlash,
+        };
+        const newPost = new PostModel(body);
+        const savedPost = await newPost.save();
 
-        const user = await ProfileModel.findById(req.body.user)
-        if (user) {
-
-            let fileNameWithSlash = req.file.filename
-            let indexOfSlash = fileNameWithSlash.indexOf("/")
-            let fileNameWithoutSlash = fileNameWithSlash.slice(indexOfSlash + 1,)
-            let body = {
-                text: req.body.text,
-                user: req.body.user,
-                image: req.file.path,
-                cloudinaryId: fileNameWithoutSlash
-            }
-            const newPost = new PostModel(body)
-            const savedPost = await newPost.save()
-
-            res.status(201).send(savedPost)
-        }
-        else {
-            next(createError(404, `User with id: ${req.body.user} not found!`))
-        }
-
-
+        res.status(201).send(savedPost);
+      } else {
+        next(createError(404, `User with id: ${req.body.user} not found!`));
+      }
     } catch (error) {
-        console.log(error);
-        next(createError(500, "An error occurred while posting with image "))
+      console.log(error);
+      next(createError(500, "An error occurred while posting with image "));
     }
-})
-
-
+  }
+);
 
 PostRouter.get("/", async (req, res, next) => {
-    try {
-        const posts = await PostModel.find()
-        if (posts.length > 0) {
-            res.send(posts)
-        } else { res.send("There are no posts yet :D ") }
-
-    } catch (error) {
-        console.log(error, "error while fetching the posts");
-        next(error)
+  try {
+    const posts = await PostModel.find({}).populate("user");
+    if (posts.length > 0) {
+      res.send(posts);
+    } else {
+      res.send("There are no posts yet :D ");
     }
-})
+  } catch (error) {
+    console.log(error, "error while fetching the posts");
+    next(error);
+  }
+});
 
 PostRouter.get("/:postId", async (req, res, next) => {
-    try {
-        const id = req.params.postId
-        const post = await PostModel.findById(id).populate("user")
-        if (post) {
-            res.send(post)
-        } else {
-            next(createError(404, `post with id: ${req.params.postId} not found!`))
-        }
-
-    } catch (error) {
-        console.log(error)
-        next(createError(500, `An error occurred while getting a specific post with id ${req.params.postId}`))
+  try {
+    const id = req.params.postId;
+    const post = await PostModel.findById(id).populate("user");
+    if (post) {
+      res.send(post);
+    } else {
+      next(createError(404, `post with id: ${req.params.postId} not found!`));
     }
-})
-
-
+  } catch (error) {
+    console.log(error);
+    next(
+      createError(
+        500,
+        `An error occurred while getting a specific post with id ${req.params.postId}`
+      )
+    );
+  }
+});
 
 PostRouter.put("/:postId", async (req, res, next) => {
-    try {
-
-        const post = await PostModel.findByIdAndUpdate(req.params.postId, req.body, {
-            runValidators: true,
-            new: true
-        })
-        if (post) {
-            res.send(post)
-        } else {
-            next(createError(404, `post with id: ${req.params.postId} not found!`))
-        }
-    } catch (error) {
-        console.log(error);
-        next(createError(500, "An error occurred while modifying the post"))
+  try {
+    const post = await PostModel.findByIdAndUpdate(
+      req.params.postId,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (post) {
+      res.send(post);
+    } else {
+      next(createError(404, `post with id: ${req.params.postId} not found!`));
     }
-
-
-})
-
-
+  } catch (error) {
+    console.log(error);
+    next(createError(500, "An error occurred while modifying the post"));
+  }
+});
 
 PostRouter.delete("/:postId", async (req, res, next) => {
-    try {
-        const post = await PostModel.findById(req.params.postId)
+  try {
+    const post = await PostModel.findById(req.params.postId);
 
-        if (post) {
-            // console.log(post + "  this is the Post");
-            if (post.cloudinaryId) {
-                console.log(post.cloudinaryId + "    this is the cloudinary id");
-                await cloudinary.uploader.destroy(post.cloudinaryId, { invalidate: true }
-                    , function (result) { console.log(result) });
-                // this just doesnt work somehow 
-            }
-            await PostModel.findByIdAndDelete(req.params.postId)
-            res.status(204).send()
-
-        } else {
-            next(createError(404, `post with id: ${req.params.postId} not found!`))
-        }
-    } catch (error) {
-        console.log(error);
-        next(createError(500, "An error occurred while deleting the post"))
+    if (post) {
+      // console.log(post + "  this is the Post");
+      if (post.cloudinaryId) {
+        console.log(post.cloudinaryId + "    this is the cloudinary id");
+        await cloudinary.uploader.destroy(
+          post.cloudinaryId,
+          { invalidate: true },
+          function (result) {
+            console.log(result);
+          }
+        );
+        // this just doesnt work somehow
+      }
+      await PostModel.findByIdAndDelete(req.params.postId);
+      res.status(204).send();
+    } else {
+      next(createError(404, `post with id: ${req.params.postId} not found!`));
     }
-})
+  } catch (error) {
+    console.log(error);
+    next(createError(500, "An error occurred while deleting the post"));
+  }
+});
 
-
-export default PostRouter
+export default PostRouter;
--- src/service/profile.js
+++ src/service/profile.js
@@ -1,174 +1,178 @@
-import {Router} from "express";
+import { Router } from "express";
 import profileModel from "../schema/profile.js";
 import createError from "http-errors";
 import multer from "multer";
-import {v2 as cloudinary} from "cloudinary";
-import {CloudinaryStorage} from "multer-storage-cloudinary";
-import {convertUrlToBase64Image} from "../helper/base64Image.js";
-import {createPDf} from "../library/pdf.js";
-import {pipeline} from "stream";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { convertUrlToBase64Image } from "../helper/base64Image.js";
+import { createPDf } from "../library/pdf.js";
+import { pipeline } from "stream";
 
 const profileRouter = Router();
 
 profileRouter.get("/profile", async (req, res, next) => {
-	try {
-		const request = await profileModel.find();
-		if (request && request.length > 0) {
-			res.send(request);
-		} else {
-			next(createError(404, "Not Found"));
-		}
-	} catch (error) {
-		next(createError(400, "Bad Request"));
-	}
+  try {
+    const request = await profileModel.find();
+    if (request && request.length > 0) {
+      res.send(request);
+    } else {
+      next(createError(404, "Not Found"));
+    }
+  } catch (error) {
+    next(createError(400, "Bad Request"));
+  }
 });
 
 profileRouter.get("/profile/:id", async (req, res, next) => {
-	try {
-		const _id = req.params.id;
-		const request = await profileModel.findById(_id);
-		if (request) {
-			res.send(request);
-		} else {
-			next(createError(404, `Profile with ${req.params.id} Id Not Found`));
-		}
-	} catch (error) {
-		if (error.name === "CastError") {
-			next(createError(404, `Profile with ${req.params.id} Id Not Found`));
-		} else {
-			next(createError(400, "Bad Request"));
-		}
-	}
+  try {
+    const _id = req.params.id;
+    const request = await profileModel.findById(_id);
+    if (request) {
+      res.send(request);
+    } else {
+      next(createError(404, `Profile with ${req.params.id} Id Not Found`));
+    }
+  } catch (error) {
+    if (error.name === "CastError") {
+      next(createError(404, `Profile with ${req.params.id} Id Not Found`));
+    } else {
+      next(createError(400, "Bad Request"));
+    }
+  }
 });
 
 profileRouter.post("/profile", async (req, res, next) => {
-	try {
-		const request = await profileModel(req.body);
-		const newProfile = await request.save();
-		if (newProfile) {
-			res.send(newProfile._id);
-		} else {
-			next(createError(400, "Bad Request"));
-		}
-	} catch (error) {
-		if (error.name === "ValidationError") {
-			const Error = {
-				message: error._message,
-				field: error.message,
-			};
-			res.send(Error);
-		} else if (error.name === "MongoError") {
-			const id = Object.keys(error.keyValue);
-			const Error = {
-				id: error.keyValue[id],
-				Code: error.code,
-				ErrorType: "Duplicating Value",
-				Advice: "Change Key Value",
-			};
-			res.send(Error);
-		} else {
-			next(createError(500, "Generic Internal Server Error"));
-		}
-	}
+  try {
+    const request = await profileModel(req.body);
+    const newProfile = await request.save();
+    if (newProfile) {
+      res.send(newProfile._id);
+    } else {
+      next(createError(400, "Bad Request"));
+    }
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const Error = {
+        message: error._message,
+        field: error.message,
+      };
+      res.send(Error);
+    } else if (error.name === "MongoError") {
+      const id = Object.keys(error.keyValue);
+      const Error = {
+        id: error.keyValue[id],
+        Code: error.code,
+        ErrorType: "Duplicating Value",
+        Advice: "Change Key Value",
+      };
+      res.send(Error);
+    } else {
+      next(createError(500, "Generic Internal Server Error"));
+    }
+  }
 });
 
 profileRouter.put("/profile/:id", async (req, res, next) => {
-	try {
-		const _id = req.params.id;
-		const request = await profileModel.findByIdAndUpdate(_id, req.body, {
-			runValidator: true,
-			new: true,
-		});
-		if (request) {
-			const response = await {_id, Operation: "updated"};
-			res.send(response);
-		} else {
-			next(createError(404, `Profile with ${req.params.id} Id Not Found`));
-		}
-	} catch (error) {
-		if (error.name === "ValidationError") {
-			next(createError(400, {error}));
-		} else {
-			next(createError(500, "Genric Internal Server Error"));
-		}
-	}
+  try {
+    const _id = req.params.id;
+    const request = await profileModel.findByIdAndUpdate(
+      _id,
+      { ...req.body },
+      {
+        runValidator: true,
+        new: true,
+      }
+    );
+    if (request) {
+      const response = await { _id, Operation: "updated" };
+      res.send(response);
+    } else {
+      next(createError(404, `Profile with ${req.params.id} Id Not Found`));
+    }
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      next(createError(400, { error }));
+    } else {
+      next(createError(500, "Genric Internal Server Error"));
+    }
+  }
 });
 
 profileRouter.delete("/profile/:id", async (req, res, next) => {
-	try {
-		const _id = req.params.id;
-		const getProfile = await findById(_id);
-		await cloudinary.uploader.destroy(
-			`${getProfile.image}`,
-			() => "IMG Deleted"
-		);
-		const request = await profileModel.findByIdAndDelete(_id);
-		if (request) {
-			const response = await {_id, Operation: "Deleted"};
-			res.send(response);
-		} else {
-			next(createError(404, `Profile with ${req.params.id} Id Not Found`));
-		}
-	} catch (error) {
-		next(createError(404, `Profile with ${req.params.id} Id Not Found`));
-	}
+  try {
+    const _id = req.params.id;
+    const getProfile = await findById(_id);
+    await cloudinary.uploader.destroy(
+      `${getProfile.image}`,
+      () => "IMG Deleted"
+    );
+    const request = await profileModel.findByIdAndDelete(_id);
+    if (request) {
+      const response = await { _id, Operation: "Deleted" };
+      res.send(response);
+    } else {
+      next(createError(404, `Profile with ${req.params.id} Id Not Found`));
+    }
+  } catch (error) {
+    next(createError(404, `Profile with ${req.params.id} Id Not Found`));
+  }
 });
 
 const cloudinaryStorage = new CloudinaryStorage({
-	cloudinary,
-	params: {
-		folder: "Profile_Picture",
-	},
+  cloudinary,
+  params: {
+    folder: "Profile_Picture",
+  },
 });
 
-const upload = multer({storage: cloudinaryStorage}).single("profile");
+const upload = multer({ storage: cloudinaryStorage }).single("profile");
 
 profileRouter.post("/profile/:id/picture", upload, async (req, res, next) => {
-	try {
-		const _id = req.params.id;
-		const request = await profileModel.findByIdAndUpdate(
-			_id,
-			{image: req.file.path},
-			{new: true}
-		);
-		if (request) {
-			const response = await {
-				_id: _id,
-				Link: req.file.path,
-				Operation: "image updated",
-			};
-			res.send(response);
-		} else {
-			next(createError(404, `Profile With ${_id} _id Not Found`));
-		}
-	} catch (error) {
-		console.log(error);
-		next(createError(500, "Server is on Fire"));
-	}
+  try {
+    const _id = req.params.id;
+    const request = await profileModel.findByIdAndUpdate(
+      _id,
+      { image: req.file.path },
+      { new: true }
+    );
+    if (request) {
+      const response = await {
+        _id: _id,
+        Link: req.file.path,
+        Operation: "image updated",
+      };
+      res.send(response);
+    } else {
+      next(createError(404, `Profile With ${_id} _id Not Found`));
+    }
+  } catch (error) {
+    console.log(error);
+    next(createError(500, "Server is on Fire"));
+  }
 });
 
 profileRouter.get("/profile/:id/cv", async (req, res, next) => {
-	try {
-		const _id = req.params.id;
-		const request = await profileModel.findById(_id);
-		if (request) {
-			const img = await convertUrlToBase64Image(request.image);
-			const source = createPDf(img, request);
-			const destination = res;
-			res.setHeader(
-				"Content-Type",
-				"application/pdf",
-				"Content-Disposition",
-				`attachment;filename=${request.name}details.pdf`
-			);
+  try {
+    const _id = req.params.id;
+    const request = await profileModel.findById(_id);
+    if (request) {
+      const img = await convertUrlToBase64Image(request.image);
+      const source = createPDf(img, request);
+      const destination = res;
+      res.setHeader(
+        "Content-Type",
+        "application/pdf",
+        "Content-Disposition",
+        `attachment;filename=${request.name}details.pdf`
+      );
 
-			pipeline(source, destination, (error) => console.log(error));
-		} else {
-			next(createError(404, `Profile With ${_id} _id Not Found`));
-		}
-	} catch (error) {
-		next(createError(500, "Server is on Fire"));
-	}
+      pipeline(source, destination, (error) => console.log(error));
+    } else {
+      next(createError(404, `Profile With ${_id} _id Not Found`));
+    }
+  } catch (error) {
+    next(createError(500, "Server is on Fire"));
+  }
 });
 
 export default profileRouter;
